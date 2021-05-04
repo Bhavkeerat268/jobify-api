351e238200d9c9e8518cc2cb387f1617bcfec315
Bhavkeerat268 <52240914+Bhavkeerat268@users.noreply.github.com>
2021-05-04 20:41:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,6 +47,8 @@ def rec(keywords: Keyword):
     final_list = json.dumps(rec_list, default=np_encoder)
 
     return {"recommend": final_list}
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 
